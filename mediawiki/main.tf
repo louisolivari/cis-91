43b4a861d2598919b6e3191cee0a54cfd363bdc5
louisolivari <lolivariii@gmail.com>
2022-12-10 22:45:47 +0000

    added external ip lines

--- mediawiki/main.tf
+++ mediawiki/main.tf
@@ -164,3 +164,11 @@ resource "google_compute_global_forwarding_rule" "default" {
 output "lb-ip" {
   value = google_compute_global_address.default.address
 }
+
+output "external-ip" {
+  value = google_compute_instance.webservers[*].network_interface[0].access_config[0].nat_ip
+}
+
+output "external-ip-db" {
+  value = google_compute_instance.db.network_interface[0].access_config[0].nat_ip
+}
